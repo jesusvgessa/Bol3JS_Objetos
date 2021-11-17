2cb787234c1b8980fe4b8af4576119c5de37b3f2
jesusvgessa <jesusvgessa@hotmail.com>
2021-11-17 20:46:22 +0100

    cambios

--- Actividad JS/js/script.js
+++ Actividad JS/js/script.js
@@ -1,15 +1,18 @@
-function Edificio(calle, num, codPos) {
+function Edificio(calle, num = "s/n", codPos) {
     //Propiedades:
     this.calle = calle;
     this.num = num;
     this.codPos = codPos;
     this.planta = new Array();
+    var sMensaje = document.createElement('p');
+    sMensaje.innerHTML = "Construido nuevo edificio en calle: " + this.calle + " nº: " + this.num + ", C.P.: " + this.codPos;
+    document.body.appendChild(sMensaje);
 
     //Metodos:
     //Añadir plantas y puertas
     this.agregarPlantasYPuertas = function(numplantas, puertas) {
         if (numplantas > 0 && puerta > 0) {
-            for (var i = 0; i <= numplantas; i++) {
+            for (var i = this.planta.length; i <= this.planta.length + numplantas; i++) {
                 var planta = i;
                 for (var f = 1; f <= puertas; f++) {
                     var puerta = f;
@@ -53,10 +56,36 @@ function Edificio(calle, num, codPos) {
         } else if (puerta < 1 || puerta > this.planta[-1][1]) {
             alert("El numero de la puerta debe ser mayor que 0 y menor que " + this.planta[-1][1]);
         } else {
-
+            var i = 0;
+            var j = 0;
+            //Recorro todos los array, y el primer numero que corresponde con la planta
+            while (this.planta[i][j] != planta) {
+                i++;
+            } //Fin Mientras
+            j++;
+            //Sigo recorriendo, pero comparando com las puertas
+            while (this.planta[i][j] != puerta) {
+                i++;
+            } //Fin Mientras
+            //Cuando la tengo, compruebo si tiene ya propietario, si tiene borro y pongo el nuevo,
+            //si no, lo añado.
+            if (this.planta[i].length == 3) {
+                this.planta[i].pop();
+                this.planta[i].push(nombre);
+            } else {
+                this.planta[i].push(nombre);
+            } //Fin Si
+            var sMensaje = document.createElement('p');
+            sMensaje.innerHTML = nombre + " es ahora el propietario de la puerta " + this.planta[i][j] + " de la planta " + this.planta[i][j - 1] + ".";
+            document.body.appendChild(sMensaje);
         } //Fin si
     };
-    this.imprimePlantas = function() {
 
+    this.imprimePlantas = function() {
+        for (var i = 0; i <= this.planta; i++) {
+            var sMensaje = document.createElement('p');
+            sMensaje.innerHTML = "Propietario del piso " + this.planta[i][1] + " de la planta " + this.planta[i][0] + ":" + this.planta[i][2] + ".";
+            document.body.appendChild(sMensaje);
+        } //Fin Para
     };
 }
